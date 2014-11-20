933bdba1fdded7d611d1b62b208cdf5174e0bb55
mygamelife <mygamelife1@gmail.com>
2014-11-20 11:50:02 +0800

    Implemented new,add,destroy linkedList

--- src/LinkedList.h
+++ src/LinkedList.h
@@ -8,8 +8,9 @@ struct LinkedList {
   void *data;
 };
 
+LinkedList *linkListNew(void *data);
 void addLinkedList(LinkedList **listPtr, LinkedList *newList);
 void removeLinkedList(LinkedList **listPtr, LinkedList *listToRemove);
-void destroyAllLinkedLists(LinkedList *fistList);
+void destroyAllLinkedLists(LinkedList *firstList);
 void findLinkedList(LinkedList **listPtr, void *data, int(*compare)(void *dataInCollection, void *data));
 #endif // LinkedList_H
